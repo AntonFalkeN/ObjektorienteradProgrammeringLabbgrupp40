b3ffea41cef83189d85943877178a3786e535cee
Nummer1InfFan <123357877+Nummer1InfFan@users.noreply.github.com>
2025-01-27 01:18:32 +0100

    Car och Volvo240

---
+++ Car.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+
+public abstract class Car {
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+
+
+    public Car(int _nrDoors, double _enginePower, Color _color, String _modelName,double _currentSpeed){
+        nrDoors = _nrDoors;
+        enginePower = _enginePower;
+        color = _color;
+        modelName = _modelName;
+        currentSpeed = _currentSpeed;
+    }
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+    public String getModelName(){return modelName;}
+    public Color getColor(){return color;}
+
+    public void setColor(Color clr){color = clr;}
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public double speedFactor(){return 0;}
+
+    protected void incrementSpeed(double amount){;}
+
+    protected void decrementSpeed(double amount){;}
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+    public static void main(String[] args){
+        Volvo240 V = new Volvo240(4, 100, Color.black, "Volvo240");
+    }
+}
