0f3c498fae3e93506eddad0003e60269b6bb9e18
Nummer1InfFan <123357877+Nummer1InfFan@users.noreply.github.com>
2025-02-04 23:41:11 +0100

    class created

---
+++ Peterbilt388CarCarrier.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.sql.Array;
+
+public class Peterbilt388CarCarrier {
+    Car[] storage;
+
+    public Peterbilt388CarCarrier(Color _color, int capacity) {
+        storage = new Car[capacity];
+    }
+}
